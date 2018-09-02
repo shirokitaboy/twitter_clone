7c3e54be536043236a46614acfc39c35c4aaca74
AkihiroKagimoto <smartphoneyou15@gmail.com>
2018-09-02 16:41:20 +0900

    change_domain_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'https://enigmatic-harbor-67623.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'enigmatic-harbor-67623.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
